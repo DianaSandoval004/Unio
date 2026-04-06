e85ca7e641e705c46ff6335cbcda198f7c0222b7
DianaSandoval004 <diana_sandoval.12@hotmail.com>
2026-04-06 17:11:26 -0600

    Actualización completa de proyecto

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM dunglas/frankenphp:php8.4.19-bookworm
-
-RUN docker-php-ext-install pdo_mysql
-
-WORKDIR /app
-COPY . .
-
-CMD sh -c 'frankenphp php-server --root /app --port $PORT'
--- Vistas/Layout/footer.php
+++
@@ -1,5 +0,0 @@
-</main>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="<?php echo BASE_URL; ?>Assets/Js/script.js"></script>
-</body>
-</html>
